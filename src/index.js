faf5f195fe5a58b777cc23ef11551745e85bbfef
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-30 16:30:48 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 var persons = [
-    { name: "Alice", job: "Font-end Dev", age: 28 },
+    {
+        name: "Alice",
+        job: "Font-end Dev",
+        age: 28,
+    },
     { name: "Bob", job: "Back-end dev", age: 44 },
     { name: "Codiku", job: "Full Stack dev", age: 30 },
 ];
-console.log(persons.map(function (person) { return "<li>".concat(person.name, "</li>"); }));
 var formatedList = persons.map(function (person) {
-    return "<li>Name : ".concat(person.name, " - Job : ").concat(person.job, "</li>");
+    return "<li>Name : ".concat(person.name, " - Job : ").concat(person.job, " - Age : ").concat(person.age, "</li>");
 });
-var htmlList = "<ul> ".concat(formatedList.join(""), " </ul>");
-document.getElementById("app").innerHTML = htmlList;
+var liListString = formatedList.join("");
+document.getElementById("app").innerHTML = "<ul> ".concat(liListString, " </ul>");
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ const persons: {
   job: string;
   age: number;
 }[] = [
-  { name: "Alice", job: "Font-end Dev", age: 28 },
+  {
+    name: "Alice",
+    job: "Font-end Dev",
+    age: 28,
+  },
   { name: "Bob", job: "Back-end dev", age: 44 },
   { name: "Codiku", job: "Full Stack dev", age: 30 },
 ];
-
-console.log(
-  persons.map((person) => `<li>${person.name}</li>`)
-);
 
 const formatedList: string[] = persons.map(
   (person: {
@@ -18,11 +18,12 @@ const formatedList: string[] = persons.map(
     job: string;
     age: number;
   }): string => {
-    return `<li>Name : ${person.name} - Job : ${person.job}</li>`;
+    return `<li>Name : ${person.name} - Job : ${person.job} - Age : ${person.age}</li>`;
   }
 );
-const htmlList: string = `<ul> ${formatedList.join(
-  ""
-)} </ul>`;
 
-document.getElementById("app")!.innerHTML = htmlList;
+const liListString: string = formatedList.join("");
+
+document.getElementById(
+  "app"
+)!.innerHTML = `<ul> ${liListString} </ul>`;
