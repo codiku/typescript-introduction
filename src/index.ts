f9b6649afda2993155d225832750e959415ac0a9
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-31 14:45:49 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
-const persons: {
+interface Person {
   name: string;
   job: string;
   age: number;
-}[] = [
+  isHappy?: boolean;
+  friendList?: Person[];
+  sayHi?: (color: string) => void;
+}
+
+const persons: Person[] = [
   {
     name: "Alice",
     job: "Font-end Dev",
     age: 28,
+    isHappy: true,
+    friendList: [
+      { age: 30, job: "Astronaut", name: "Tobby" },
+    ],
+    sayHi: (color: string) => {
+      console.log(
+        "Hi i am Alice ! my favorite color is ",
+        color
+      );
+    },
   },
   { name: "Bob", job: "Back-end dev", age: 44 },
   { name: "Codiku", job: "Full Stack dev", age: 30 },
 ];
 
+persons[0].sayHi?.("red");
+
 const formatedList: string[] = persons.map(
-  (person: {
-    name: string;
-    job: string;
-    age: number;
-  }): string => {
+  (person: Person): string => {
     return `<li>Name : ${person.name} - Job : ${person.job} - Age : ${person.age}</li>`;
   }
 );
