d9c689ff7676634d3908cb86089336cec7a86573
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-01 18:51:10 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+enum Direction {
+  North = "North",
+  South = "South",
+  Est = "Est",
+  West = "West",
+}
 
+const direction: Direction = Direction.West;
+
+function move(direction: Direction) {
+  console.log("Moving ", direction);
+}
+move(direction);
