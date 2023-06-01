2e33bf9c1b5d059376a900e04a28b128b3e20bb5
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-01 16:44:47 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 class BankAccount {
   private balance: number;
+  private interestRate: number;
+  private interestCeiling: number;
+  private favoriteAccounts: BankAccount[] = [];
+  private id: number;
 
-  constructor(balance: number) {
+  constructor(
+    id: number,
+    balance: number,
+    interestRate: number,
+    interestCeiling: number
+  ) {
+    this.id = id;
     this.balance = balance;
+    this.interestRate = interestRate;
+    this.interestCeiling = interestCeiling;
   }
 
   deposit(amount: number): void {
@@ -16,22 +28,47 @@ class BankAccount {
     this.balance -= amount;
   }
 
-  getBalance() {
+  getBalance(): number {
     return this.balance;
   }
+
+  transferMoney(amount: number, account: BankAccount): void {
+    this.widthdraw(amount);
+    account.deposit(amount);
+  }
+
+  getMensualInterest(): number {
+    if (this.balance > this.interestCeiling) {
+      return this.interestCeiling * this.interestRate;
+    } else {
+      return this.balance * this.interestRate;
+    }
+  }
+  addAccountToFavorites(account: BankAccount): void {
+    this.favoriteAccounts.push(account);
+  }
+
+  getFavoriteAccounts(): BankAccount[] {
+    return this.favoriteAccounts;
+  }
+
+  removeFavoriteAccountById(id: number): void {
+    const indexToRemove = this.favoriteAccounts.findIndex(
+      (account: BankAccount) => account.id === id
+    );
+    if (indexToRemove === -1) {
+      throw new Error("Account not found in favorites");
+    }
+    this.favoriteAccounts.splice(indexToRemove, 1);
+  }
 }
-const account1 = new BankAccount(40000);
 
-account1.deposit(10000);
+const account1 = new BankAccount(1, 40000, 0.01, 50000);
+const account2 = new BankAccount(2, 100000, 0.01, 50000);
+account1.addAccountToFavorites(account2);
 
-console.log("balance", account1.getBalance());
+account1.removeFavoriteAccountById(2);
 
-account1.widthdraw(50000);
+console.log(account1.getFavoriteAccounts().length);
 
-console.log("balance after widthdraw", account1.getBalance());
-
-try {
-  account1.widthdraw(1);
-} catch (err: unknown) {
-  console.log((err as Error).message);
-}
+account1.removeFavoriteAccountById(2);
