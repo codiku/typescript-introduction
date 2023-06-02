9fb83f7a49b5b6394562efeaad72632473f51e49
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-02 13:49:15 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,7 @@
-interface MovieInterface {
-  title: string;
-  year: number;
-  isForAdult: boolean;
-}
-
-interface MovieInterface {
-  director: string;
-}
-/*
-interface Vehicule {
+/*interface Vehicule {
   maxSpeed: number;
   brand: string;
 }
-*/
-type Vehicule = {
-  maxSpeed: number;
-  brand: string;
-};
 
 interface Car extends Vehicule {
   horsePower: number;
@@ -26,44 +11,22 @@ interface Boat extends Vehicule {
   sails: number;
 }
 
-type Bike = {
-  pedals: number;
-} & Vehicule;
-
 let car: Car = {
   brand: "Mustang",
-  horsePower: 300,
-  maxSpeed: 250,
+  horsePower: 250,
+  maxSpeed: 280,
 };
-let boat: Boat = {
-  brand: "Apex",
-  sails: 3,
-  maxSpeed: 250,
-};
+*/
 
-type MovieType = {
-  title: string;
-  year: number;
-  isForAdult: boolean;
-};
+interface Vehicule {
+  maxSpeed: number;
+  brand: string;
+}
 
-type MovieType = {
-  director: string;
-};
+type Car = {
+  horsePower: number;
+} & Vehicule;
 
-let m1: MovieInterface;
-
-let m2: MovieType;
-
-m1 = {
-  isForAdult: true,
-  title: "Fight club",
-  year: 1999,
-  director: "David Fincher",
-};
-
-m2 = {
-  isForAdult: true,
-  title: "Fight club",
-  year: 1999,
-};
+type Boat = {
+  sails: number;
+} & Vehicule;
