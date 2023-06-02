b55134e30f10da3cecafce97e53be04bc022bdd9
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-02 12:22:01 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,40 @@
-enum Direction {
-  North = "North",
-  South = "South",
-  Est = "Est",
-  West = "West",
+// Unions
+
+let value: number | boolean | string;
+
+value = "Hi";
+
+interface Square {
+  kind: "square";
+  size: number;
 }
 
-const direction: Direction = Direction.West;
-
-function move(direction: Direction) {
-  console.log("Moving ", direction);
+interface Circle {
+  kind: "circle";
+  radius: number;
 }
-move(direction);
+
+let myShape: Square | Circle;
+
+myShape = {
+  kind: "square",
+  size: 10,
+};
+
+function displayShape(shape: Square | Circle): void {
+  console.log("Kind ", shape.kind);
+  if (shape.kind === "circle") {
+    console.log("Radius ", shape.radius);
+  } else {
+    console.log("Size ", shape.size);
+  }
+}
+
+displayShape(myShape);
+
+let data: (number | string | boolean | (Square | Circle))[] =
+  [1, "Hi", true, myShape];
+
+if (typeof data[0] === "number") {
+  console.log("Indeed first element is a number !");
+}
