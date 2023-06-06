a0c165535cad0fcc1ca7d708b07336861925d850
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-06 16:14:22 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
-interface Vehicule {
-  maxSpeed: number;
-  brand: string;
+class Circle {
+  private _radius: number;
+
+  constructor(radius: number) {
+    this._radius = radius;
+  }
+
+  set radius(radius: number) {
+    if (radius > 0) {
+      this._radius = radius;
+    } else {
+      throw new Error("Cannot set a negative radius");
+    }
+  }
+
+  get radius(): number {
+    return this._radius;
+  }
 }
 
-type Car = {
-  horsePower: number;
-} & Vehicule;
+const circle = new Circle(5);
 
-type Boat = {
-  sails: number;
-} & Vehicule;
+circle.radius = -2;
 
-let car: Car;
-let boat: Boat;
+console.log(circle.radius);
