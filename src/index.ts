4daa8c0c9d9691ea2bcbb3c86b0007852254a1b6
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-08 11:39:22 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,26 @@ interface Bird {
 
 function getRandomPet(): Fish | Bird {
   if (Math.random() > 0.5) {
-    return { swim: () => console.log("I swimmm") };
+    return { swim: () => console.log("Swimming") };
   } else {
     return {
-      fly: () => console.log("I fly"),
-      walk: () => console.log("I walk"),
+      fly: () => console.log("Flying"),
+      walk: () => console.log("Walking"),
     };
   }
 }
+
 function isFish(pet: Fish | Bird): pet is Fish {
   return "swim" in pet;
 }
+
 const pet = getRandomPet();
 
 if (isFish(pet)) {
+  console.log("It's a fish");
   pet.swim();
 } else {
+  console.log("It's a bird");
   pet.fly();
   pet.walk();
 }
