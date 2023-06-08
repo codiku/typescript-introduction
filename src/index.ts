02be81db65b6459277a1d7f90e4a6ffddfa61f76
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-06-08 11:32:09 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,29 @@
-abstract class Spell {
-  private _name: string;
-
-  constructor(name: string) {
-    this._name = name;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  abstract cast(): void;
+interface Fish {
+  swim: () => void;
+}
+interface Bird {
+  fly: () => void;
+  walk: () => void;
 }
 
-enum FireSpellName {
-  FireBolt = "Fire Bolt",
-  FireWall = "Fire Wall",
-  BigBang = "Big Bang",
-}
-
-enum FrostSpellName {
-  FrostBolt = "Frost Bolt",
-  Blizzard = "Blizzard",
-}
-
-class FireSpell extends Spell {
-  readonly burningDamage = 20;
-  constructor(name: FireSpellName) {
-    super(name);
-  }
-  cast() {
-    console.log(
-      this.name,
-      ` - Boom you are burning the enemy ! It took ${this.burningDamage} damages`
-    );
+function getRandomPet(): Fish | Bird {
+  if (Math.random() > 0.5) {
+    return { swim: () => console.log("I swimmm") };
+  } else {
+    return {
+      fly: () => console.log("I fly"),
+      walk: () => console.log("I walk"),
+    };
   }
 }
-
-class FrostSpell extends Spell {
-  readonly slowingRate = 0.5;
-
-  constructor(name: FrostSpellName) {
-    super(name);
-  }
-
-  cast() {
-    console.log(
-      this.name,
-      ` - Brrr you are freezing the enemy,  it's slowed by ${this.slowingRate}`
-    );
-  }
+function isFish(pet: Fish | Bird): pet is Fish {
+  return "swim" in pet;
 }
+const pet = getRandomPet();
 
-type SpellName<S> = S extends FireSpell
-  ? FireSpellName
-  : FrostSpellName;
-
-class Wizard<S extends Spell> {
-  private spellBook: S[] = [];
-
-  constructor(spellBook: S[]) {
-    this.spellBook = spellBook;
-  }
-
-  castAllAtOnce() {
-    this.spellBook.forEach((spell: S) => {
-      spell.cast();
-    });
-  }
-
-  castFromSpellBook(name: SpellName<S>) {
-    const spell = this.spellBook.find(
-      (spell) => spell.name == name
-    );
-    if (spell) {
-      spell.cast();
-    } else {
-      throw new Error(
-        "You don't have this spell in your spell book !"
-      );
-    }
-  }
+if (isFish(pet)) {
+  pet.swim();
+} else {
+  pet.fly();
+  pet.walk();
 }
-
-const frostSpells: FrostSpell[] = [
-  new FrostSpell(FrostSpellName.FrostBolt),
-  new FrostSpell(FrostSpellName.Blizzard),
-];
-
-const frostWizard = new Wizard(frostSpells);
-
-frostWizard.castFromSpellBook(FireSpellName.BigBang);
